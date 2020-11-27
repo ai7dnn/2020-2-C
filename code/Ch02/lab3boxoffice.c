dace6001cee43c75008fbfd0fb3c1fba8e46b20e
ai7dnn <70050528+ai7dnn@users.noreply.github.com>
2020-11-28 08:42:40 +0900

    upload

---
+++ code/Ch02/lab3boxoffice.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	//영화 정보 구조체 
+	typedef struct movie
+	{
+		char* title;		//영화제목
+		int attendance;		//관객수
+		char director[20];	//감독
+	} movie;
+
+	movie box[] = { { "명량", 17613000, "김한민" },
+					{ "극한직업", 16261000, "윤제균" },
+					{ "신과함께-죄와벌", 14419000 } };
+
+	//세번째 영화의 감독을 김용화로 저장
+	strcpy(box[2].director, "김용화");
+
+	printf("       제목        감독   관객수\n");
+	printf("=================================\n");
+	for (int i = 0; i < 3; i++)
+		printf("[%15s] %6s %d\n",
+			box[i].title, box[i].director, box[i].attendance);
+
+	return 0;
+}
+
